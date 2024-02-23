56eedd9e7da22b11e5b15f3279fdd74dce6a9b0c
Jan Havlin <241030@vutbr.cz>
2024-02-23 11:49:38 +0100

    fix

--- encryptor_client.cpp
+++ encryptor_client.cpp
@@ -22,8 +22,8 @@
 #include <cryptopp/dh.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <openssl-3.2.1/openssl/ssl.h>
-#include <openssl-3.2.1/openssl/err.h>
+#include <openssl-3.2.1/openssl-3.2.1/include/openssl/ssl.h>
+#include <openssl-3.2.1/openssl-3.2.1/include/openssl/ssl.h>
 
 #define PORT 62000
 #define KEYPORT 61000
