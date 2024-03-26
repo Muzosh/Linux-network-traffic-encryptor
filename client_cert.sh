88b19abe1453fa523dea5f71dc56270068a7ed77
Jan Havlin <241030@vutbr.cz>
2024-03-26 19:28:30 +0100

    fix

--- client_cert.sh
+++ client_cert.sh
@@ -2,6 +2,8 @@
 
 rm -rf client.crt client.csr client.key
 
+echo "Removed old client certificates"
+
 openssl genpkey -algorithm RSA -out client.key -aes256
 openssl req -new -key client.key -out client.csr
 openssl x509 -req -in client.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out client.crt -days 365
